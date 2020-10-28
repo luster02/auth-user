0cb3a4039c1a5cf995e61ce3d9fed6ece9ddfac5
luster02 <bombolti_02@hotmail.com>
2020-10-28 11:16:51 -0600

    [ADD]: user-microservice dto

---
+++ src/dto/signup.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+
+export class SignupDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}

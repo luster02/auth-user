f18c7eb27718e5a4327b636562e3ac1b2c845dcf
luster02 <bombolti_02@hotmail.com>
2020-10-28 11:15:22 -0600

    [ADD]: user-microservice config module

---
+++ src/config/config.interface.ts
@@ -0,0 +1,4 @@
+export interface IEnvironmentVariables {
+    MONGODB_URI: string;
+    JWT_SECRET: string;
+}
---
+++ src/config/config.provider.ts
@@ -0,0 +1,4 @@
+export default () => ({
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET
+})
